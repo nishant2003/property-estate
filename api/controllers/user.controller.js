00557586a1ffad8e0a471ac7230948a1261f25ce
nishant2003 <108606959+nishant2003@users.noreply.github.com>
2024-07-21 04:10:39 +0530

    api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,11 @@
+export const test = (req, res) => {
+    res.json({
+        message: "Hello Lodo",
+    });
+    // res.send("Bhadwe");
+};
+// module.exports.test = (req, res) => {
+//     res.send('User data');
+//   };
+  
+  
