63065232d3c80c3463aed7df7e0c5c65b0db8bdc
nishant2003 <108606959+nishant2003@users.noreply.github.com>
2024-07-27 23:11:38 +0530

    complete Listing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,29 +4,32 @@ import Signup from "./pages/SignUp";
 import Signin from "./pages/Signin";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
 
 export default function App() {
-  return(
-    
+  return (
     <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path="/" element = {<Home/>}/>
-      <Route path="/signup" element={<Signup/>}/>
-      <Route path = "/about" element={<About/>}/>
-      <Route path = "/signin" element={<Signin/>}/>
-      <Route path = "/about" element={<About/>}/>
-      
-      <Route element={<PrivateRoute/>}>
-      <Route path = "/profile" element={<Profile/>}/>
-      <Route path = "/create-listing" element={<CreateListing/>}/>
-      </Route>
-      
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/about" element={<About />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
