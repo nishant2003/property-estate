ed2fe5c1d7d5e14c50def94878809d0f3775b77a
nishant2003 <108606959+nishant2003@users.noreply.github.com>
2024-08-15 14:44:46 +0530

    Docker done

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,74 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+// import { nodePolyfills } from 'vite-plugin-node-polyfills'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server:{
+//     proxy:{
+//       '/api':{
+//         target: 'http://localhost:3000',
+//         changeOrigin: true,
+//         secure: false,
+//     },
+//   },
+// },
+//   plugins: [react(),
+//     nodePolyfills({
+//       include: ['crypto', 'process', 'stream', 'util'],
+//       globals: { global: true, process: true },
+//     }),      
+//   ],
+// })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+// import { nodePolyfills } from 'vite-plugin-node-polyfills'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server:{
+//     proxy:{
+//       '/api':{
+//         target: 'http://localhost:3000',
+//         changeOrigin: true,
+//         secure: false,
+//     },
+//     host:'0.0.0.0',
+//     port:5173,
+//   },
+// },
+//   plugins: [react(),
+//     nodePolyfills({
+//       include: ['crypto', 'process', 'stream', 'util'],
+//       globals: { global: true, process: true },
+//     }),      
+//   ],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
+  server: {
+    proxy: {
+      '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
+        prependPath: true, // Ensure this line is correct
+      },
     },
   },
-},
-  plugins: [react(),
+  plugins: [
+    react(),
     nodePolyfills({
       include: ['crypto', 'process', 'stream', 'util'],
       globals: { global: true, process: true },
-    }),      
+    }),
   ],
-})
+});
