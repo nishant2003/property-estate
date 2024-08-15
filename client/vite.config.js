5b8de31860d6f31ed1d656919213d551330ad188
nishant2003 <108606959+nishant2003@users.noreply.github.com>
2024-08-15 15:38:55 +0530

    Final Docker commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -22,30 +22,7 @@
 // })
 
 
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-// import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   server:{
-//     proxy:{
-//       '/api':{
-//         target: 'http://localhost:3000',
-//         changeOrigin: true,
-//         secure: false,
-//     },
-//     host:'0.0.0.0',
-//     port:5173,
-//   },
-// },
-//   plugins: [react(),
-//     nodePolyfills({
-//       include: ['crypto', 'process', 'stream', 'util'],
-//       globals: { global: true, process: true },
-//     }),      
-//   ],
-// })
 
 
 import { defineConfig } from 'vite';
@@ -57,10 +34,12 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        // target: 'http://localhost:3000',
+        target: 'http://node-api:3000',
         changeOrigin: true,
         secure: false,
-        prependPath: true, // Ensure this line is correct
+        // rewrite: (path) => path.replace(/^\/api/, ''),
+        // prependPath: true, // Ensure this line is correct
       },
     },
   },
