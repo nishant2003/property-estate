b77eafa981f83af1dc19f6c69b74b61a35fb5c9e
nishant2003 <108606959+nishant2003@users.noreply.github.com>
2024-09-06 02:42:31 +0530

    little error resolve

--- api/index.js
+++ api/index.js
@@ -1,46 +1,44 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
-import userRoute from './routes/user.route.js'
-import authRoute from './routes/auth.route.js'
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
-import { errorHandler } from './utils/error.js';
-import cors from 'cors';
-import listingRoute from './routes/listing.route.js';
 import path from 'path';
-
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((err) => {
-    console.log(err)
-});
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log('Connected to MongoDB!');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 const __dirname = path.resolve();
 
-
 const app = express();
-app.use(cors());
-app.use(cookieParser());
+
 app.use(express.json());
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-})
+app.use(cookieParser());
 
-app.use('/api/user', userRoute);
-app.use('/api/auth', authRoute);
-app.use('/api/listing', listingRoute);
+app.listen(3000, () => {
+    console.log('Server is running on port 3000!');
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
+
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client','dist','index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
-
-
-
-
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
@@ -51,3 +49,61 @@ app.use((err, req, res, next) => {
         message,
     });
 });
+
+
+
+
+
+// import express from 'express';
+// import mongoose from 'mongoose';
+// import dotenv from 'dotenv'
+// import userRoute from './routes/user.route.js'
+// import authRoute from './routes/auth.route.js'
+// import cookieParser from 'cookie-parser';
+// import { errorHandler } from './utils/error.js';
+// import cors from 'cors';
+// import listingRoute from './routes/listing.route.js';
+// import path from 'path';
+
+// dotenv.config();
+
+// mongoose.connect(process.env.MONGO).then(() => {
+//     console.log('Connected to MongoDB');
+// }).catch((err) => {
+//     console.log(err)
+// });
+
+// const __dirname = path.resolve();
+
+
+// const app = express();
+// app.use(cors());
+// app.use(cookieParser());
+// app.use(express.json());
+
+// app.listen(3000, () => {
+//     console.log('Server is running on port 3000');
+// })
+
+// app.use('/api/user', userRoute);
+// app.use('/api/auth', authRoute);
+// app.use('/api/listing', listingRoute);
+
+// app.use(express.static(path.join(__dirname, '/client/dist')));
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'client','dist','index.html'));
+// })
+
+
+
+
+
+// app.use((err, req, res, next) => {
+//     const statusCode = err.statusCode || 500;
+//     const message = err.message || 'Internal Server Error';
+//     return res.status(statusCode).json({
+//         success: false,
+//         statusCode,
+//         message,
+//     });
+// });
